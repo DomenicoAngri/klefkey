5a5f7bd15e0430cb70b7b48e5a4ff3259ba5c3a1
Domenico Angri <domenico.angri@soprasteria.com>
2017-08-29 18:08:39 +0200

    Prova effettuata con DB json e libreria gson.

--- src/it/dax/klefkey/offline/control/Main.java
+++ src/it/dax/klefkey/offline/control/Main.java
@@ -1,6 +1,55 @@
 package it.dax.klefkey.offline.control;
 
+import com.google.gson.Gson;
+import it.dax.klefkey.offline.model.PasswordJsonDb;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Main{
+
+    public static void main(String args[]){
+        List<PasswordJsonDb> db = new ArrayList<PasswordJsonDb>();
+        Gson gson = new Gson();
+
+        PasswordJsonDb entry1 = new PasswordJsonDb();
+        entry1.setPasswordKey("Prova 1");
+        entry1.setPasswordValue("ProvaOne1");
+
+        PasswordJsonDb entry2 = new PasswordJsonDb();
+        entry2.setPasswordKey("Prova 2");
+        entry2.setPasswordValue("ProvaTwo2");
+
+        db.add(entry1);
+        db.add(entry2);
+
+        String dbJson = gson.toJson(db);
+        System.out.println(dbJson);
+
+        List<PasswordJsonDb> input = new ArrayList<PasswordJsonDb>();
+        input = gson.fromJson(dbJson, ArrayList.class);
+
+        System.out.println(input.toString());
+
+        Map<String,String> ciao = new HashMap<String,String>();
+
+        ciao.put("prova","cazzo");
+        ciao.put("prova","figa");
+
+        ciao.isEmpty();
+
+
+        System.out.println(ciao.get((String)"prova"));
+
+
+
+
+    }
+
+
+
+
 
 
 
---
+++ src/it/dax/klefkey/offline/model/PasswordJsonDb.java
@@ -0,0 +1,26 @@
+package it.dax.klefkey.offline.model;
+
+public class PasswordJsonDb{
+
+    private String passwordKey;
+    private String passwordValue;
+
+    public String getPasswordKey() {
+        return passwordKey;
+    }
+
+    public void setPasswordKey(String passwordKey) {
+        this.passwordKey = passwordKey;
+    }
+
+    public String getPasswordValue() {
+        return passwordValue;
+    }
+
+    public void setPasswordValue(String passwordValue) {
+        this.passwordValue = passwordValue;
+    }
+
+    // TODO: toString()
+
+}
